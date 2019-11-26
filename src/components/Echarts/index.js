fdb942c018074715cb2c23794d0bfd2ce5dc7904
how <370969878@qq.com>
2019-11-26 17:15:43 +0800

    Echart 最大最小值显示问题

--- src/components/Echarts/index.js
+++ src/components/Echarts/index.js
@@ -3,9 +3,28 @@ import { View, StyleSheet, Platform } from 'react-native';
 import renderChart from './renderChart';
 import echarts from './echarts.min';
 import WebView from 'react-native-webview';
+import { isFunction } from '../../../../../src/utils/DataTypeUtil';
 
-const source = Platform.OS == 'ios' ? require('./tpl.html') : { 'uri': 'file:///android_asset/echarts/tpl.html' };
+let source = Platform.OS == 'ios' ? require('./tpl.html') : { 'uri': 'file:///android_asset/echarts/tpl.html' };
 export default class App extends Component {
+
+  /**add by david at 2019-11-26 start  */
+  constructor(props) {
+    super(props);
+    let { type } = this.props
+    if (!!type && type == 'funnel') {
+      source = Platform.OS == 'ios' ? require('./tpl_funnel.html') : { 'uri': 'file:///android_asset/echarts/tpl_funnel.html' };
+    } else {
+      source = Platform.OS == 'ios' ? require('./tpl.html') : { 'uri': 'file:///android_asset/echarts/tpl.html' };
+    }
+  }
+  handleOnLoad() {
+    if (this.props.loadFinishCallback && isFunction(this.props.loadFinishCallback)) {
+      this.props.loadFinishCallback()
+    }
+  }
+
+  /**add by david at 2019-11-26 end  */
   componentWillReceiveProps(nextProps) {
     if (nextProps.option !== this.props.option) {
       this.refs.chart.reload();
@@ -16,6 +35,7 @@ export default class App extends Component {
     return (
       <View style={{ flex: 1, height: this.props.height || 400, }}>
         <WebView
+          onLoad={() => { this.handleOnLoad() }}
           originWhitelist={['*']}//RN0.56开始，本地的html加载 添加白名单
           ref="chart"
           // scalesPageToFit={false}
